f1aa21f9f9c6b366b59847e8e382fe1bce706fc1
MondraDev89 <jose.mondragondev.1@gmail.com>
2025-08-02 14:29:48 -0600

    Declaracion de variables en Dart/DartBasics

--- Dart/dart_basics/bin/dart_basics.dart
+++ Dart/dart_basics/bin/dart_basics.dart
@@ -1,5 +1,48 @@
 import 'package:dart_basics/dart_basics.dart' as dart_basics;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_basics.calculate()}!');
+  // Declaracion de variables
+  // var name = "Beto";
+  // var age = 20;
+  // var example = 34.6;
+  // print(name);
+  // print(age);
+  // print(example);
+  
+  //Tipos de datos numericos
+  int age = 31;
+  int test = 23;
+
+  double large = 345.7;
+  double triangle = 24.5;
+
+  num age2 = 12;
+  num age3 = 16.3;
+
+  //Tipos de datos String
+  String name = 'Juan';
+  name = "Juanca";
+  name = 'Mariana';
+  String currentAge = "26 años";
+
+  // String fullText = name + currentAge;
+  String fullText = 'Soy $name y tengo $currentAge'; //concatenar strings
+  print(fullText);
+
+
+  //variables booleanas
+  bool imHappy = true;
+
+  //Tipo dinamico
+  dynamic example = "Hola mundo";
+  print(example);
+  example = 23;
+  print(example);
+
+  //Tipos fijos
+  final String example2 = "David"; //tiempo ejecucion
+  const String example3 = "Medrano"; //tiempo de compilacion
 }
+
+
+
